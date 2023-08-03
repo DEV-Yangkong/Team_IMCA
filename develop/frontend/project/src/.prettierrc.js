fcddcacb55d0a4ef9a2d9bf5cabf6870fcee6cd5
hiheeen <gmldms0338@naver.com>
2023-08-03 12:56:26 +0900

    Header 구현

--- develop/frontend/project/src/.prettierrc.js
+++ develop/frontend/project/src/.prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 80,
-  tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 80,
+    tabWidth: 2,
 };
--- develop/frontend/project/src/App.js
+++ develop/frontend/project/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Header from './components/Header/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/MainPage/Main';
+import Musical from './components/ConcertPage/Musical';
+import Act from './components/ConcertPage/Act';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Test</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/concert_act" element={<Act />} />
+          <Route path="/concert_musical" element={<Musical />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

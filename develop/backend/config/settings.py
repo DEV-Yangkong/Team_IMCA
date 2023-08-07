194060d203641cdfbbeb47d47f03abb2a0d7965a
jinwoo <sds7629>
2023-08-07 19:13:43 +0900

    API

--- develop/backend/config/settings.py
+++ develop/backend/config/settings.py
@@ -43,6 +43,7 @@ THIRDPARTY_APPS = [
     "common.apps.CommonConfig",
     "youtube_videos.apps.YoutubeVideosConfig",
     "corsheaders",
+    "apis.apps.ApisConfig",
 ]
 
 DEFAULT_APPS = [
@@ -155,6 +156,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3080",
--- develop/backend/config/urls.py
+++ develop/backend/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/youtube_videos/", include("youtube_videos.urls")),
+    path("API/", include("apis.urls")),
 ]

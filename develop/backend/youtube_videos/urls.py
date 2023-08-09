fa16ff293964e07d2ec75848153aea36f54d7e20
yangkong <dev.yangkong@gmail.com>
2023-08-09 15:46:27 +0900

    Update urls.py

--- develop/backend/youtube_videos/urls.py
+++ develop/backend/youtube_videos/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 from . import views
 
-# from .views import CountResult
-
 urlpatterns = [
     path("", views.Youtube_Videos.as_view()),
     path("<int:pk>/", views.Youtube_VideoDetail.as_view()),
-    # ------------------------------------------ 추가한 코드 👇🏻 - 예은 -
-    path("<int:pk>/", views.Youtube_VideoDetail.as_view()),
-    # ------------------------------------------ 추가한 코드 ☝🏻 - 예은 -
     # path(
     #     "count/",
     #     CountResult.as_view(),

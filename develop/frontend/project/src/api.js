eae9a225aec6ccad98eb160c566b04fe11fdc3a7
hiheeen <gmldms0338@naver.com>
2023-08-10 20:15:46 +0900

    데이터 검색기능 구현

--- develop/frontend/project/src/api.js
+++ develop/frontend/project/src/api.js
@@ -4,17 +4,18 @@ import { xml2js } from 'xml-js';
 // const instance = () => {
 //   axios.create({ baseURL: 'http://localhost:8000/API' });
 // };
-export const getAllMusical = () => {
+export const getAllMusical = (type, startDate, endDate) => {
   //전체 게시글을 가져오는 API
+
   return axios
     .get('http://localhost:8000/API/public', {
       params: {
         cpage: 1,
         rows: 30,
-        shcate: 'GGGA',
-        prfstate: '01',
-        prfpdfrom: '20230801',
-        prfpdto: '20230831',
+        shcate: type === '연극' ? 'AAAA' : 'GGGA',
+        prfstate: '02',
+        prfpdfrom: startDate || '',
+        prfpdto: endDate || '',
       },
     })
     .then((res) => {
@@ -25,25 +26,55 @@ export const getAllMusical = () => {
     })
     .catch((error) => console.log('err', error));
 };
-export const getAllAct = () => {
+
+// export const getAllAct = () => {
+//   return axios
+//     .get('http://localhost:8000/API/public', {
+//       params: {
+//         cpage: 1,
+//         rows: 30,
+//         shcate: 'AAAA',
+//         prfstate: '01',
+//         prfpdfrom: '20230801',
+//         prfpdto: '20231030',
+//       },
+//     })
+//     .then((res) => {
+//       // console.log(res.data);
+//       const options = { compact: true, spaces: 2 };
+//       const result = xml2js(res.data, options);
+
+//       console.log('actArray', result.dbs.db);
+//       return result.dbs.db;
+//     })
+//     .catch((error) => console.log('err', error));
+// };
+
+export const getMusicalBoxOffice = () => {
   return axios
-    .get('http://localhost:8000/API/public', {
+    .get('http://localhost:8000/API/boxoffice', {
       params: {
-        cpage: 1,
-        rows: 30,
-        shcate: 'AAAA',
-        prfstate: '01',
-        prfpdfrom: '20230801',
-        prfpdto: '20230831',
+        catecode: 'AAAA',
       },
     })
     .then((res) => {
-      // console.log(res.data);
       const options = { compact: true, spaces: 2 };
       const result = xml2js(res.data, options);
-
-      console.log('actArray', result.dbs.db);
-      return result.dbs.db;
-    })
-    .catch((error) => console.log('err', error));
+      console.log('boxofficeM', result);
+      return result.boxofs.boxof.slice(0, 5);
+    });
 };
+// useEffect(() => {
+//   axios
+//     .get('http://localhost:8000/API/boxoffice', {
+//       params: {
+//         catecode: 'AAAA',
+//       },
+//     })
+//     .then((res) => {
+//       const options = { compact: true, spaces: 2 };
+//       const result = xml2js(res.data, options);
+//       console.log('boxofficeA', result);
+//       setBoxOfAct(result.boxofs.boxof.slice(0, 5));
+//     });
+// }, []);

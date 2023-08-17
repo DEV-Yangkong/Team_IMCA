7aeabb42c3fbdf037c76bf697cd3bf17df372689
tanmtn <tanmtni@gmail.com>
2023-08-17 17:36:09 +0900

    게시판 api
    게시판 api 연결 시도중

--- develop/frontend/project/src/components/CommunityPage/Editor.js
+++ develop/frontend/project/src/components/CommunityPage/Editor.js
@@ -1,11 +1,21 @@
 import { useNavigate } from 'react-router-dom';
+import styles from './Editor.module.css';
 
 const Editor = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h2>글쓰기</h2>
+    <div className={styles.Editor}>
+      <h2>자유게시판</h2>
+      <div>
+        <button
+          className={styles.EditorCancel}
+          type="button"
+          onClick={() => navigate(-1)}
+        >
+          취소
+        </button>
+      </div>
     </div>
   );
 };
